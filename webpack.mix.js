d155bf83dc87c9e9d14d1bac7ad003ba17cd7d04
iphone-taro <saga.siga.noga@gmail.com>
2022-11-10 05:25:08 +0900

    no message

--- webpack.mix.js
+++ webpack.mix.js
@@ -9,8 +9,10 @@ const mix = require('laravel-mix');
  | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
- */
 
 mix.js('resources/js/app.js', 'public/js')
     .vue()
     .sass('resources/sass/app.scss', 'public/css');
+ */
+
+mix.sass('resources/sass/theme.scss', 'public/app/css');
